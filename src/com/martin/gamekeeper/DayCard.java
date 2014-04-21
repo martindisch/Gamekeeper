8eabf146ae49b9700d83389747af5e87e2f2c3fb
Martin Disch <martindisch@gmail.com>
2014-04-21 17:14:17 +0200

    Working half-clever solution

--- src/com/martin/gamekeeper/DayCard.java
+++ src/com/martin/gamekeeper/DayCard.java
@@ -1,7 +1,10 @@
 package com.martin.gamekeeper;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
@@ -12,19 +15,31 @@ public class DayCard extends LinearLayout {
 	public DayCard(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DayCard, 0, 0);
-		try {
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			inflater.inflate(R.layout.daycard_layout, this, true);
-			String[] days = context.getResources().getStringArray(R.array.days);
-			((TextView) findViewById(R.id.tvDay)).setText(days[a.getInt(R.styleable.DayCard_day, 0)]);
-			((TextView) findViewById(R.id.tvScore)).setText(a.getString(R.styleable.DayCard_candidate1Score) + " : " + a.getString(R.styleable.DayCard_candidate2Score));
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.card, this, true);
+		String[] days = context.getResources().getStringArray(R.array.days);
+		((TextView) findViewById(R.id.tvDay)).setText(days[a.getInt(R.styleable.DayCard_day, 0)]);
+		((TextView) findViewById(R.id.tvScore)).setText(a.getString(R.styleable.DayCard_candidate1Score) + " : " + a.getString(R.styleable.DayCard_candidate2Score));
+		
+		Drawable background = null;
+		switch (a.getInt(R.styleable.DayCard_day, 0)) {
+		case 0:
+			background = getResources().getDrawable(R.drawable.card_background_blue);
+			break;
+		case 1:
+			background = getResources().getDrawable(R.drawable.card_background_purple);
+			break;
+		case 2:
+			background = getResources().getDrawable(R.drawable.card_background_green);
+			break;
+		case 3:
+			background = getResources().getDrawable(R.drawable.card_background_orange);
+			break;
+		case 4:
+			background = getResources().getDrawable(R.drawable.card_background_red);
+			break;
 		}
-		catch (Exception e) {
-			
-		}
-		finally {
-			a.recycle();
-		}
+		findViewById(R.id.llCardRoot).setBackgroundDrawable(background);
 	}
 
 }
--- src/com/martin/gamekeeper/MainActivity.java
+++ src/com/martin/gamekeeper/MainActivity.java
@@ -1,11 +1,9 @@
 package com.martin.gamekeeper;
 
 import android.app.Activity;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,10 +11,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		View view = findViewById(R.id.container);
-		GradientDrawable shape = (GradientDrawable) view.getBackground();
-		shape.setColor(getResources().getColor(android.R.color.holo_blue_bright));
 	}
 
 	@Override
